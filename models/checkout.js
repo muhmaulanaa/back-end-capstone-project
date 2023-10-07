aee6abf941bf4c00c89da738d1efb0fe69500456
fatilaa <fmardhati@gmail.com>
2023-10-07 09:14:06 +0700

    Fix model checkout

--- models/checkout.js
+++ models/checkout.js
@@ -1,2 +1,34 @@
 
-module.exports = Checkout;
+const { DataTypes } = require("sequelize");
+const conn = require("../db").conn; // Import koneksi database
+
+const Checkout = conn.define(
+  "Checkout",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    totalAmount: { type: DataTypes.DECIMAL(10, 0), allowNull: false },
+    address: { type: DataTypes.STRING, allowNull: false },
+    paymentMethod: { type: DataTypes.STRING, allowNull: false },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
+
+module.exports = {
+  Checkout,
+};
+
